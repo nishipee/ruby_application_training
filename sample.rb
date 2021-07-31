15cb1306c3f6cf63eb9c06d9cec44d7ba6decbc9
nishipee <sn.haryo.000@gmail.com>
2021-07-31 21:20:08 +0900

    rubyトレーニング3

--- sample.rb
+++ sample.rb
@@ -1,10 +1,5 @@
-require "date"
-
-week_day = ["日", "月", "火", "水", "木", "金", "土"]
-
-if week_day == 5
-  puts "今日は#{week_day[Date.today.wday]}曜日だ！"
-else
-  puts "今日は#{week_day[Date.today.wday]}曜日"
+unless a + b < 0
+  puts "計算結果は0より大きいです"
 end
+
 
