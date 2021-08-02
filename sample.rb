9ed39d72dd8d8cfe61b2f60663e2a009daba5643
nishipee <sn.haryo.000@gmail.com>
2021-08-02 21:43:49 +0900

    rubyトレーニング5

--- sample.rb
+++ sample.rb
@@ -1,6 +1,15 @@
-sum = 0
-10.times do |i|
-  sum += i + 1
+def search(target_num, input)
+  input.each_with_index do |num, i|
+    if num == target_num
+      puts "#{i + 1}番目にあります"
+      return
+    end
+  end
+  puts "その数は含まれていません"
 end
 
-puts sum
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+
+search(5, input)
+search(12, input)
+search(7, input)
