c14f91a2f70e7ffd7e9af0286ac54630eaba5525
nishipee <sn.haryo.000@gmail.com>
2021-08-05 21:59:39 +0900

    rubyトレーニング7-修正

--- sample.rb
+++ sample.rb
@@ -1,8 +1,10 @@
 fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
-apple_total_price = fruits_price[0][1][0] + fruits_price[0][1][1] + fruits_price[0][1][2]
-orange_total_price = fruits_price[1][1][0] + fruits_price[1][1][1] + fruits_price[1][1][2]
-melon_total_price = fruits_price[2][1][0] + fruits_price[2][1][1] + fruits_price[2][1][2]
 
-each.fruits_price do |fruit|
-  puts "#{fruit[0]}の合計金額は#{"
+fruits_price.each do |fruit|
+  sum = 0
+  fruit[1].each do |price|
+    sum += price
+  end
+  puts "#{fruit[0]}の合計金額は#{sum}円です"
+end
   
