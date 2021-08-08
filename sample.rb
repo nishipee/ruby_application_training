6b17f68c4379dc2d54ba735882e622cb7635f6b8
nishipee <sn.haryo.000@gmail.com>
2021-08-08 18:04:32 +0900

    rubyトレーニング10

--- sample.rb
+++ sample.rb
@@ -1,11 +1,23 @@
-def design(s)
-  str = "+" * (s.length + 2)
-  if s.length >= 1 && s.length <= 100
-    puts str
-    puts "+#{s}+"
-    puts str
+class Fruit
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.fresh
+    puts "採れたて新鮮な果実です"
+  end
+
+  def introduce
+    puts "#{@name}は#{@price}円です"
   end
 end
 
-s = gets.chomp
-design(s)
+apple = Fruit.new("りんご", 120)
+orange = Fruit.new("オレンジ", 200)
+strawbery = Fruit.new("イチゴ", 60)
+
+Fruit.fresh
+apple.introduce
+orange.introduce
+strawbery.introduce
