d3ed15043029c2d8300bcf91f01cda23bb01973d
nishipee <sn.haryo.000@gmail.com>
2021-08-09 12:20:07 +0900

    rubyトレーニング12

--- sample.rb
+++ sample.rb
@@ -1,9 +1,8 @@
-def density(n, m)
-  if (n >= 1 && n <= 100) && (m >= 1 && m <= 100)
-    puts n * m
+def eve(n)
+  if n >= 2 && n <= 31
+    puts n - 1
   end
 end
 
 n = gets.to_i
-m = gets.to_i
-density(n, m)
+eve(n)
