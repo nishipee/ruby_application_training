a54e80fdb62fae54925b5f4734be654ac00be90f
nishipee <sn.haryo.000@gmail.com>
2021-08-20 11:12:12 +0900

    rubyトレーニング21

--- sample.rb
+++ sample.rb
@@ -1,12 +1,19 @@
-def num(a, b, c)
-  ab = a + b
-  if c <= 3
-    puts ab / c
-  elsif c >= 4
-    puts ab* c
+def calculate_points(amount, is_birthday)
+  if amount >= 1000
+    if is_birthday
+      puts "ポイントは#{(amount * 0.05 * 5).to_i}点です"
+    else
+      puts "ポイントは#{(amount * 0.05).to_i}点です"
+    end
+  else
+    if is_birthday
+      puts "ポイントは#{(amount * 0.03 * 5).to_i}点です"
+    else
+      puts "ポイントは#{(amount * 0.03).to_i}点です"
+    end
   end
 end
 
-num(1,5,3)
-num(1,5,5)
-num(1,9,-2)
+calculate_points(500, false)
+calculate_points(2000, false)
+calculate_points(3000, true)
