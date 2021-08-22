b9be4e209556914ef74856ed9612e2e60c5a1a81
nishipee <sn.haryo.000@gmail.com>
2021-08-22 09:52:36 +0900

    rubyトレーニング23

--- sample.rb
+++ sample.rb
@@ -1,18 +1,17 @@
-def fizz_buzz
-  num = 1
-  while (num <= 100) do
-    if (num % 3 == 0) && (num % 5 == 0)
+def fizzbuzz(max_num)
+  (1..max_num).each do |num|
+    if (num % 3 == 0 ) && (num % 5 == 0)
       puts "FizzBuzz"
-    elsif (num % 3) == 0
+    elsif num % 3 == 0
       puts "Fizz"
-    elsif (num % 5) == 0
+    elsif num % 5 == 0
       puts "Buzz"
     else
       puts num
     end
-
-    num = num + 1
   end
 end
 
-fizz_buzz
+puts 'いくつまで数えますか？'
+num = gets.to_i
+fizzbuzz(num)
