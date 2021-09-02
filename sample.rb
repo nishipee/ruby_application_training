07417d83adac97cfbc43ab94ad69b49568273058
nishipee <sn.haryo.000@gmail.com>
2021-09-02 10:02:13 +0900

    rubyトレーニング32

--- sample.rb
+++ sample.rb
@@ -1,18 +1,15 @@
-def near_ten(num)
-  hundred = num / 100
-  tenth = (num % 100) / 10
-  first = num % 10
-  total = hundred + tenth + first
-  if total % 10 >= 8 || total % 10 <= 2
+def xyz_there(str)
+  if str.include?(".xyz")
+    puts "False"
+  elsif str.include?("xyz")
     puts "True"
-  elsif total % 10 <= 5
-    puts "10の倍数との差は#{total % 10}です"
-  elsif total % 10 <= 7
-    puts "10の倍数との差は#{(10 - total % 10).abs}です"
+  else
+    puts "False"
   end
 end
 
-
-near_ten(117)
-near_ten(123)
-near_ten(111)
+xyz_there('abcxyz')
+xyz_there('abc.xyz')
+xyz_there('xyz.abc')
+xyz_there('azbycx')
+xyz_there('a.zbycx')
