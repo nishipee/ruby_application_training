83af822686a3c46d95b4923bfd58c2f5e5aa3b92
nishipee <sn.haryo.000@gmail.com>
2021-09-09 12:50:44 +0900

    rubyトレーニング35

--- sample.rb
+++ sample.rb
@@ -1,4 +1,13 @@
-10.times do |i|
-  puts "#{i + 1}回目の繰り返し"
+def info(movie, str)
+  puts movie[str]
 end
 
+puts  "以下から一つを選んで入力してください。
+・title
+・genre
+・year"
+
+movie = { title: "ハリーポッター", genre: "ファンタジー", year: "2001年" }
+str = gets.chomp.to_sym
+
+info(movie, str)
